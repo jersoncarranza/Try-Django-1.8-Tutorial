ec2962116d445565a39205e5b83d4968bb898e50
jerson@hotmail.com <jerson@hotmail.com>
2015-07-13 16:18:43 -0500

    Tutorial 11 - Model Form - Learn

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from app.models import SignUp
+from .forms import SignUpForm
 # Register your models here.
 
 class SignUpAdmin(admin.ModelAdmin):
 	#list_display = ["__unicode__","timestamp","updated"] #python 2.7
 	list_display = ["__str__","timestamp","updated"] #3.3
-	class Meta:
-		model = SignUp
+	form = SignUpForm
+	#class Meta:
+	#	model = SignUp
 
 admin.site.register(SignUp, SignUpAdmin)
