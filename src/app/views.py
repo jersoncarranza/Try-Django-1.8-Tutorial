cbf74c7b166bc1e43aa1289a3ffba37efb42cd25
jerson@hotmail.com <jerson@hotmail.com>
2015-07-14 12:19:06 -0500

    tutorial 12

--- src/app/views.py
+++ src/app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request, "home.html", {})
+	title = 'Welcome'
+	if request.user.is_authenticated():
+		title="My title %s" %(request.user)
+
+	context={
+		"template_title":title,
+	}
+	return render(request, "home.html", context)
